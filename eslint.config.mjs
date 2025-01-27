59bab74366a7e1c81adbb010a52c89487c7e5524
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-27 21:47:06 +0100

    🔧 (eslint.config.mjs): add "plugin:tailwindcss/recommended" to the eslintConfig array to enable Tailwind CSS linting rules ⬆️ (package.json): upgrade "@tailwindcss/forms" to version "^0.5.10" ⬆️ (package.json): upgrade "next" to version "15.1.6" ⬆️ (package.json): upgrade "react-icons" to version "^5.4.0" 🔧 (tailwind.config.mjs): add "@tailwindcss/forms" plugin to the plugins array in the Tailwind CSS configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  "plugin:tailwindcss/recommended",
+];
 
 export default eslintConfig;
