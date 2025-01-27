2052c8a5143cf21eea1e0ca789efb1ab5d1eb712
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-27 22:12:52 +0100

    ✨ (sideBar.js): create a new component SideBar to display a sidebar in the dashboard 📝 (sideBar.js): import and render the User component inside the SideBar component

---
+++ src/app/components/dashboard/sidebar/sideBar.js
@@ -0,0 +1,9 @@
+import User from "./user/user";
+
+export default function SideBar() {
+  return (
+    <div className="bg-white h-screen w-1/5  shadow-md p-4 ">
+      <User />
+    </div>
+  );
+}
