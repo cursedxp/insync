a0e1fb93e8b20f687a6338d8e37899384d981caa
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-28 12:46:49 +0100

    ✨ (layout.js): add Chat component to the DashboardLayout to display a chat feature in the dashboard ♻️ (layout.js): refactor the layout structure to use flexbox and adjust the styling to improve the layout ✨ (page.js): add Chat component to the Dashboard page to display a chat feature in the dashboard ♻️ (page.js): refactor the return statement to use a flex container for better layout structure and adjust the styling

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,9 +1,12 @@
 import SideBar from "@/app/components/dashboard/sidebar/sideBar";
-
+import Chat from "@/app/components/chat/chat";
 export default function DashboardLayout({ children }) {
   return (
-    <div className="bg-gray-100 flex">
-      <SideBar />
+    <div className="bg-gray-100 w-screen ">
+      <div className="p-4 h-screen flex gap-4">
+        <SideBar />
+        <Chat />
+      </div>
       {children}
     </div>
   );
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,14 +1,8 @@
 "use client";
 import SearchPanel from "@/app/components/dashboard/sidebar/search/searchPanel";
-import ModalContainer from "@/app/components/common/modalContainer/modalContainer";
+import Chat from "@/app/components/chat/chat";
 import { useSelector } from "react-redux";
-
 export default function Dashboard() {
   const isOpen = useSelector((state) => state.search.isOpen);
-  return (
-    <div>
-      Dashboard
-      {isOpen && <SearchPanel />}
-    </div>
-  );
+  return <div className="flex">{isOpen && <SearchPanel />}</div>;
 }
