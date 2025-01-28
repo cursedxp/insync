346853a26808e950c7cd8abb5f96f07c9a9aca1f
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-28 12:46:23 +0100

    ✨ (sideBar.js): add new component "Users" to the sidebar to display a list of users ♻️ (sideBar.js): refactor the CSS classes of the sidebar to use a rounded-2xl border radius for a more visually appealing design

--- src/app/components/dashboard/sidebar/sideBar.js
+++ src/app/components/dashboard/sidebar/sideBar.js
@@ -1,10 +1,12 @@
 import User from "./user/user";
 import Search from "./search/search";
+import Users from "./users/users";
 export default function SideBar() {
   return (
-    <div className="bg-white h-screen w-1/5 shadow-md p-4 flex flex-col gap-4">
+    <div className="bg-white w-1/5 shadow-md flex flex-col gap-4 rounded-2xl">
       <User />
       <Search />
+      <Users />
     </div>
   );
 }
