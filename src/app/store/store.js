9e19929641fd7e9b07ec9d633b9b30120dc60bd2
Anil Ozsoy <anilozsoy@gmail.com>
2025-01-28 19:50:22 +0100

    ✨ (store.js): add chatReducer to the store configuration to enable chat functionality in the app

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./slices/searchSlice";
+import chatReducer from "./slices/chatSlice";
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    chat: chatReducer,
   },
 });
