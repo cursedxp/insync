0fe6999cc1c0436ec7b63748518db103aa25fea6
Anil Ozsoy <anilozsoy@gmail.com>
2025-02-06 15:39:28 +0100

    ✨ (chatSlice.js): add support for toggling the detailsOpen state to show or hide the details section in the chat ✨ (chatSlice.js): add support for closing the files section in the chat ✨ (chatSlice.js): add support for setting the current tab in the chat to switch between different tabs

--- src/app/store/slices/chatSlice.js
+++ src/app/store/slices/chatSlice.js
@@ -2,20 +2,30 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   detailsOpen: false,
+  filesOpen: false,
+  currentTab: "chat",
 };
 
 const chatSlice = createSlice({
   name: "chat",
   initialState,
   reducers: {
-    setDetailsOpen: (state) => {
-      state.detailsOpen = true;
+    toggleDetails: (state) => {
+      state.detailsOpen = !state.detailsOpen;
     },
     setDetailsClose: (state) => {
       state.detailsOpen = false;
     },
+
+    setFilesClose: (state) => {
+      state.filesOpen = false;
+    },
+    setCurrentTab: (state, action) => {
+      state.currentTab = action.payload;
+    },
   },
 });
 
-export const { setDetailsOpen, setDetailsClose } = chatSlice.actions;
+export const { toggleDetails, setDetailsClose, setFilesClose, setCurrentTab } =
+  chatSlice.actions;
 export default chatSlice.reducer;
