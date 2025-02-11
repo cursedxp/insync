916c76eeaf7f796a79a1a91101cadd48d8faca87
Anil Ozsoy <anilozsoy@gmail.com>
2025-02-11 11:03:36 +0100

    📝 (conversationBar.js): add animation to ConversationBar component using framer-motion library 📝 (sideBar.js): add animation to SideBar component using framer-motion library 📝 (layout.js): conditionally render Chat component based on the currentFunctionTab state value

--- src/app/components/dashboard/sidebar/sideBar.js
+++ src/app/components/dashboard/sidebar/sideBar.js
@@ -2,6 +2,7 @@
 import FunctionBar from "./functionbar/functionBar";
 import ConversationBar from "./coversationbar/conversationBar";
 import { useSelector } from "react-redux";
+import { AnimatePresence } from "framer-motion";
 export default function SideBar() {
   const currentFunctionTab = useSelector(
     (state) => state.app.currentFunctionTab
@@ -9,7 +10,9 @@ export default function SideBar() {
   return (
     <div className="bg-white shadow-md flex justify-center rounded-2xl">
       <FunctionBar />
-      {currentFunctionTab === "chat" && <ConversationBar />}
+      <AnimatePresence>
+        {currentFunctionTab === "chat" && <ConversationBar />}
+      </AnimatePresence>
     </div>
   );
 }
--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,11 +1,16 @@
+"use client";
 import SideBar from "@/app/components/dashboard/sidebar/sideBar";
 import Chat from "@/app/components/chat/chat";
+import { useSelector } from "react-redux";
 export default function DashboardLayout({ children }) {
+  const currentFunctionTab = useSelector(
+    (state) => state.app.currentFunctionTab
+  );
   return (
     <div className="bg-gray-100 w-screen ">
       <div className="p-4 h-screen flex gap-4">
         <SideBar />
-        <Chat />
+        {currentFunctionTab === "chat" && <Chat />}
       </div>
       {children}
     </div>
